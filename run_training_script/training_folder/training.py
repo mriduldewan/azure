57ff9d87367156543fbe8de12b035663907940bc
Mridul <mridul.dewan@outlook.com>
2024-04-07 13:04:31 +1000

    Added params into the script

--- run_training_script/training_folder/training.py
+++ run_training_script/training_folder/training.py
@@ -1,6 +1,13 @@
 import urllib.request
 import pandas as pd
 import numpy as np
+import argparse
+import joblib
+import os
+from azureml.core import Run
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
 
 url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/diabetes.csv"
 
@@ -12,11 +19,6 @@ urllib.request.urlretrieve(url, file_path)
 diabetes = pd.read_csv(file_path)
 diabetes.head()
 
-from azureml.core import Run
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
 # Get experiment run context
 run = Run.get_context()
 
@@ -24,13 +26,18 @@ run = Run.get_context()
 feature_cols = ['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'Age']
 label_col = ['Diabetic']
 
+# Split into train and test
 X, y = diabetes[feature_cols].values, diabetes[label_col].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
 print(f"Training Set size: {len(X_train)}\nTest Set size = {len(X_test)}\n\n")
 
+# Set regularization hyperparameter
+parser = argparse.ArgumentParser()
+parser.add_argument('--reg-rate', type=float, dest='reg_rate', default=0.01)
+args = parser.parse_args()
+reg = args.reg_rate
+
 # Train the model
-reg= 0.1
 model = LogisticRegression(C=1/reg, solver='liblinear')
 model.fit(X_train, y_train.flatten())
 
@@ -40,7 +47,6 @@ acc = np.average(y_hat == y_test)
 run.log('Accuracy', float(acc))
 
 # Save the trained model
-import os
 os.makedirs('outputs', exist_ok=True)
 joblib.dump(value=model, filename='outputs/model.pkl')
 
